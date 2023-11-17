b134221819b99fc5b51f92e671ac432d5efefbca
liptonzuma <solomonboateng231@gmail.com>
2023-11-17 11:30:01 +0000

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Poppins_400Regular',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ src/utils/fonts.ts
@@ -0,0 +1,10 @@
+export const regular = {
+  fontFamily: 'Poppins_400Regular',
+};
+export const semiBold = {
+  fontFamily: 'Poppins_600SemiBold',
+};
+
+export const bold = {
+  fontFamily: 'Poppins_700Bold',
+};
